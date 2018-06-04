e51788dd6d962e46d8f58d1cb78dc273b1c19927
N0N3N <39456383+N0N3N@users.noreply.github.com>
2018-06-04 19:32:21 +0300

    gsfgds

--- src/test/java/Locator.java
+++ src/test/java/Locator.java
@@ -1,15 +1,17 @@
 import core.CoreFunc;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class Locator {
 
-    CoreFunc core =  new CoreFunc();
+    private CoreFunc core =  new CoreFunc();
 
     @Test
     public void firstTest() {
 
         core.openWebPage("https://rus.delfi.lv");
 
+        core.findElement(By.xpath("//img[@alt='DELFI']"));
 
         core.closeWebPage();
 
